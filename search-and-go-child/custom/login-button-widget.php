b241df3d0282b79e6b3e1442658186953147fcaa
Linda "zig" Speight <linda@reachmaine.com>
2017-07-24 17:28:16 -0400

    turn off zlogin widget.

--- search-and-go-child/custom/login-button-widget.php
+++ search-and-go-child/custom/login-button-widget.php
@@ -3,16 +3,16 @@
  * Example Widget Class
  */
 class zlogin_widget extends WP_Widget {
- 
- 
+
+
     /** constructor -- name this the same as the class above */
     function zlogin_widget() {
-        parent::WP_Widget(false, $name = 'zigs Login Button Widget');	
+        parent::WP_Widget(false, $name = 'zigs Login Button Widget');
     }
- 
+
     /** @see WP_Widget::widget -- do not rename this */
     /* when widget is displayed on front end. */
-    function widget($args, $instance) {	
+    function widget($args, $instance) {
         extract( $args );
         $title 		= apply_filters('widget_title', $instance['title']);
         if ($instance['message']) {
@@ -20,7 +20,7 @@ class zlogin_widget extends WP_Widget {
         } else {
           $message = 'Sign In';
         }
-        
+
         ?>
         <?php echo $before_widget; ?>
         <?php if ( $title )
@@ -30,15 +30,15 @@ class zlogin_widget extends WP_Widget {
           <?php global $user_ID;
             global $current_user;
             get_currentuserinfo();
-            if('' == $user_ID){   
+            if('' == $user_ID){
               //use shortcode as easiest way to style button
               echo do_shortcode('[eltd_button size="medium" type="outline" text="'.$message.'" custom_class="" icon_pack="font_awesome" fa_icon="" link="'.wp_login_url().'" target="_self" ]');
-            
+
             } else { ?>
               <div class="eltd-logged-in-user">
                 <span>
-                <?php 
-                // show username inside button 
+                <?php
+                // show username inside button
                  echo do_shortcode('[eltd_button size="medium" type="outline" text="'.$current_user->display_name.'" custom_class="" icon_pack="font_awesome" fa_icon="" link="'.(wp_logout_url( $_SERVER["REQUEST_URI"] )).'" target="_self" ]');
                   ?>
                 </span>
@@ -48,34 +48,34 @@ class zlogin_widget extends WP_Widget {
           <?php echo $after_widget; ?>
         <?php
     }
- 
+
     /** @see WP_Widget::update -- do not rename this */
-    function update($new_instance, $old_instance) {		
+    function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
 		$instance['message'] = strip_tags($new_instance['message']);
         return $instance;
     }
- 
+
     /** @see WP_Widget::form -- do not rename this */
     /*  backend form */
-    function form($instance) {	
- 
+    function form($instance) {
+
         $title 		= esc_attr($instance['title']);
         $message	= esc_attr($instance['message']);
         ?>
          <p>
-          <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+          <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
           <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
         </p>
 		<p>
-          <label for="<?php echo $this->get_field_id('message'); ?>"><?php _e('Button Text (defaults Sign In)'); ?></label> 
+          <label for="<?php echo $this->get_field_id('message'); ?>"><?php _e('Button Text (defaults Sign In)'); ?></label>
           <input class="widefat" id="<?php echo $this->get_field_id('message'); ?>" name="<?php echo $this->get_field_name('message'); ?>" type="text" value="<?php echo $message; ?>" />
         </p>
-        <?php 
+        <?php
     }
- 
- 
+
+
 } // end class example_widget
-add_action('widgets_init', create_function('', 'return register_widget("zlogin_widget");'));
+//add_action('widgets_init', create_function('', 'return register_widget("zlogin_widget");'));
 ?>
