83215dcd4a4be9cd7845bf2d00b6c9c61ac2625f
linda@reachmaine.com <linda@reachmaine.com>
2018-04-02 13:55:59 -0400

    add styling for woocommerce products

--- search-and-go-child/custom/woocommerce.php
+++ search-and-go-child/custom/woocommerce.php
@@ -16,4 +16,16 @@ add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
 	}
   // remove categories printed at bottom of single product page.
   remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+
+    /*
+  Disable Variable Product Price Range completely:
+  */
+
+  add_filter( 'woocommerce_variable_sale_price_html', 'my_remove_variation_price', 10, 2 );
+  add_filter( 'woocommerce_variable_price_html', 'my_remove_variation_price', 10, 2 );
+
+  function my_remove_variation_price( $price ) {
+      $price = '';
+      return $price;
+    }
 ?>
